97709ce4acd2556c74140c629c2f039fe0daba8d
Jerry Treeman <35148370+TieBeTie@users.noreply.github.com>
2023-03-03 21:53:50 +0300

    Update PolymorphicMapper.hpp

--- task2/PolymorphicMapper.hpp
+++ task2/PolymorphicMapper.hpp
@@ -2,11 +2,38 @@
 
 #include <optional>
 
-
 template <class From, auto target>
-struct Mapping;
+struct Mapping
+{
+  using Target = decltype(target);
+};
 
 template <class Base, class Target, class... Mappings>
-struct PolymorphicMapper {
-  static std::optional<Target> map(const Base& object);
+  requires(std::same_as<
+               std::remove_cv_t<typename Mappings::Target>,
+               Target> &&
+           ...)
+struct PolymorphicMapper
+{
+  static std::optional<Target> map(const Base &)
+  {
+    return {};
+  }
+};
+
+template <class Base, class Target, Target tg, class Cast, class... Mappings>
+  requires std::is_base_of_v<Base, Cast>
+struct PolymorphicMapper<Base, Target, Mapping<Cast, tg>, Mappings...>
+{
+  static std::optional<Target> map(const Base &object)
+  {
+    if (dynamic_cast<const Cast *>(std::addressof(object)) != nullptr)
+    {
+      return {tg};
+    }
+    else
+    {
+      return PolymorphicMapper<Base, Target, Mappings...>::map(object);
+    }
+  }
 };
